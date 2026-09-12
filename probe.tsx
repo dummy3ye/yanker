160f98c46e5e75f1408cf2b899df92712185499c
イムティヤズ <reza1234khan1234@gmail.com>
2026-09-12 15:04:47 +0600

    remove leftover probe scratch file

--- probe.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import {renderToString} from 'ink'
-import {Logo} from './src/components/logo.tsx'
-const out = renderToString(<Logo themeMode="dark" />)
-console.log('PLAIN>>>\n' + out.replace(/\u001b\[[0-9;]*m/g, ''))
